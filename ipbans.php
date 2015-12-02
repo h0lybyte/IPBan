d9b39ffd5ed452b189709258ac8c697a10799800
h0ly byte <admin@kbve.com>
2015-12-02 23:13:36 +0000

    Reorganizing to Exile Concept

--- ipbans.php
+++ ipbans.php
@@ -13,8 +13,28 @@
         @$data = @h0lybyte();
         @$command = @$data->command;
         $exile = '5';
-        $member_data = $ipbwi->member->findMemberCustomFieldValue($exile, true);
-        $final = array("status" => true, "data" =>         ;
+        $m_data = $ipbwi->member->findMemberCustomFieldValue($exile, true);
+        
+        $final = array();
+        if(is_array($m_data) && count($m_data) > 0){
+                                                                        foreach($m_data as $M){
+                                                                        $FPM = array(
+                                                                                        'name' => $PM['name'], 
+                                                                                        'map_last_topic_reply' => $PM['map_last_topic_reply'],
+                                                                                        'map_topic_id' => $PM['map_topic_id'],
+                                                                                        'mt_title' => $PM['mt_title']);
+                                                                        array_push($final, $FPM);
+                                   
+                                    }
+                                                                }
+
+
+
+
+        
+        
+        
+        $final = array("status" => true, "data" => $member_data);
         echo json_encode($final, JSON_UNESCAPED_SLASHES);
         exit;
 ?>
