11ac6e01306fce9d84f3b95774597529cef1f091
h0ly byte <admin@kbve.com>
2015-12-03 17:06:33 +0000

    Fixing commands

--- ipbans.php
+++ ipbans.php
@@ -18,9 +18,16 @@
         }
         if($command == "ipbans")
         {
-                $m_data = $ipbwi->member->getList(array('group' => '5'));
-                
+                //$options = array('group' => '5');
+                //$m_data = $ipbwi->member->getList($options);
+                $i = 5; // Group for Bans
+                $where = 'm.member_group_id="'.$i.'" ';
+                $where = 'WHERE m.member_id != "0" AND ('.$where.')';
+                $ipbwi->ips_wrapper->DB->query('SELECT m.*, g.*, cf.* FROM '.$ipbwi->board['sql_tbl_prefix'].'members m LEFT JOIN '.$ipbwi->board['sql_tbl_prefix'].'groups g ON (m.member_group_id=g.g_id) LEFT JOIN '.$ipbwi->board['sql_tbl_prefix'].'pfields_content cf ON (cf.member_id=m.member_id) '.$where.';');
                 $final = array();
+                while($row = $ipbwi->ips_wrapper->DB->fetch()){
+                        $m_data[$row['member_id']] = $row;
+                  }
                 if(is_array($m_data) && count($m_data) > 0){
                                                                                 foreach($m_data as $M){
                                                                                 $data = $ipbwi->member->info($M);
